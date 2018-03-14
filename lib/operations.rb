424192ee1f8a23be02a54cddf8ced6f257d6d6c2
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-14 21:55:02 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,11 @@
 def unsafe?(speed)
 
-	if 40 < speed
-		return false
-	elsif speed > 60
+	if (40 < speed ||  speed > 60)
 		return false
 	else
 		return true
 	end
-
 end
-
-
 
 def not_safe?(speed)
 
